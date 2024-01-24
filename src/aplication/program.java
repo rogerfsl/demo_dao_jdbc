ec431f0a00a50c3cfdabfd9d6ad824f287031cdf
rogerfsl <rogerfsl@hotmail.com>
2024-01-24 17:12:22 -0300

    update implementation

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -35,18 +35,24 @@ public class program {
 		
 		System.out.println("\n=== TEST 3: seller findByAll ===");
 		
-		
 		list = sellerDao.findAll();
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert ===");
+		
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000, department);
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller update ===");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
