5827db815c16026548d352aaef423add12aa25d7
rogerfsl <rogerfsl@hotmail.com>
2024-01-24 10:57:26 -0300

    Seller entity class

---
+++ src/aplication/program.java
@@ -0,0 +1,21 @@
+package aplication;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000, obj);
+		
+		System.out.println(seller);
+
+	}
+
+}
